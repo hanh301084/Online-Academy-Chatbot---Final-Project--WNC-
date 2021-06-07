357e9984d310db43dbbe82bf6b6134cd59cc86a5
Giang070999 <55496731+Giang070999@users.noreply.github.com>
2021-06-07 15:59:44 +0700

    updated app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+app.get("/", function(req, res) {
+    res.json({
+        message: "hello api"
+    });
+})
+app.use('/api/courses', require('./routes/course.route'));
+
+const PORT = 8000;
+app.listen(PORT, function() {
+    console.log('Online Course api is running');
+})
+
 
